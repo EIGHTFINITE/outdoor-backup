edf02013625e82a02840f0fab6a482007c896204
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2026-04-09 11:07:15 +0200

    Initial commit

---
+++ .github/workflows/actions-artifacts.sh
@@ -0,0 +1,233 @@
+#!/bin/bash
+# Execution starts in .github/workflows/artifacts.yml
+export GIT_AUTHOR_DATE="$(git log -1 --format=%aD)"
+export GIT_COMMITTER_DATE="$(git log -1 --format=%cD)"
+
+# 7z Windows x64
+p7zip_version=$(cat pagelist.json | python -c "import sys, json; print(json.load(sys.stdin)['config']['p7zip-version'])")
+p7zip_short=$(cat pagelist.json | python -c "import sys, json; print(json.load(sys.stdin)['config']['p7zip-short'])")
+curl -sSLo "7z$p7zip_short-x64.exe" --header "Authorization: token $GITHUB_TOKEN" https://github.com/ip7z/7zip/releases/download/$p7zip_version/7z$p7zip_short-x64.exe
+mkdir -p "bin/windows/x64/7z/7z$p7zip_short-x64"
+7z x -o"bin/windows/x64/7z/7z$p7zip_short-x64" "7z$p7zip_short-x64.exe" | grep "ing archive"
+rm "7z$p7zip_short-x64.exe"
+rm "bin/windows/x64/7z/7z$p7zip_short-x64/Uninstall.exe"
+sed -i '/\/bin\//d' -- '.gitignore'
+git add "bin/windows/x64/7z/7z$p7zip_short-x64"
+git -c user.name="GitHub" -c user.email="noreply@github.com" commit --author="github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>" -m"Add Windows x64 7z $p7zip_version release artifacts" | sed -n 1p
+git checkout -- '.gitignore'
+if [[ $(git status --porcelain | tee /dev/stderr | head -c1 | wc -c) -ne 0 || $(git clean -dffx | tee /dev/stderr | head -c1 | wc -c) -ne 0 ]]
+  then exit 1
+fi
+
+# PortableGit Windows x64
+portable_git_short=$(cat pagelist.json | python -c "import sys, json; print(json.load(sys.stdin)['config']['portable-git-short'])")
+portable_git_version=$(cat pagelist.json | python -c "import sys, json; print(json.load(sys.stdin)['config']['portable-git-version'])")
+curl -sSLo "PortableGit-$portable_git_short-64-bit.7z.exe" --header "Authorization: token $GITHUB_TOKEN" https://github.com/git-for-windows/git/releases/download/v$portable_git_version/PortableGit-$portable_git_short-64-bit.7z.exe
+mkdir -p "bin/windows/x64/git/PortableGit-$portable_git_short-64-bit"
+7z x -o"bin/windows/x64/git/PortableGit-$portable_git_short-64-bit" "PortableGit-$portable_git_short-64-bit.7z.exe" | grep "ing archive"
+rm "PortableGit-$portable_git_short-64-bit.7z.exe"
+rm "bin/windows/x64/git/PortableGit-$portable_git_short-64-bit/post-install.bat"
+rmdir "bin/windows/x64/git/PortableGit-$portable_git_short-64-bit/dev/mqueue"
+rmdir "bin/windows/x64/git/PortableGit-$portable_git_short-64-bit/dev/shm"
+rmdir "bin/windows/x64/git/PortableGit-$portable_git_short-64-bit/tmp"
+git submodule add -f -- https://github.com/EIGHTFINITE/void.git "bin/windows/x64/git/PortableGit-$portable_git_short-64-bit/dev/mqueue"
+git submodule add -f -- https://github.com/EIGHTFINITE/void.git "bin/windows/x64/git/PortableGit-$portable_git_short-64-bit/dev/shm"
+git submodule add -f -- https://github.com/EIGHTFINITE/void.git "bin/windows/x64/git/PortableGit-$portable_git_short-64-bit/tmp"
+sed -i '/\/bin\//d' -- '.gitignore'
+git add .gitmodules "bin/windows/x64/git/PortableGit-$portable_git_short-64-bit"
+git -c user.name="GitHub" -c user.email="noreply@github.com" commit --author="github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>" -m"Add Windows x64 PortableGit $portable_git_short release artifacts" | sed -n 1p
+git checkout -- '.gitignore'
+if [[ $(git status --porcelain | tee /dev/stderr | head -c1 | wc -c) -ne 0 || $(git clean -dffx | tee /dev/stderr | head -c1 | wc -c) -ne 0 ]]
+  then exit 1
+fi
+
+# uBlock Origin
+ublock_origin_version=$(cat pagelist.json | python -c "import sys, json; print(json.load(sys.stdin)['config']['ublock-origin-version'])")
+curl -sSLo "uBlock0_$ublock_origin_version.chromium.zip" --header "Authorization: token $GITHUB_TOKEN" https://github.com/gorhill/uBlock/releases/download/$ublock_origin_version/uBlock0_$ublock_origin_version.chromium.zip
+mkdir -p "extensions"
+7z x -o"extensions" "uBlock0_$ublock_origin_version.chromium.zip" | grep "ing archive"
+rm "uBlock0_$ublock_origin_version.chromium.zip"
+sed -i '/\/extensions\//d' -- '.gitignore'
+git add "extensions/uBlock0.chromium"
+git -c user.name="GitHub" -c user.email="noreply@github.com" commit --author="github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>" -m"Add uBlock Origin $ublock_origin_version release artifacts" | sed -n 1p
+git checkout -- '.gitignore'
+if [[ $(git status --porcelain | tee /dev/stderr | head -c1 | wc -c) -ne 0 || $(git clean -dffx | tee /dev/stderr | head -c1 | wc -c) -ne 0 ]]
+  then exit 1
+fi
+
+# Node Linux x64
+node_version=$(cat package.json | python -c "import sys, json; print(json.load(sys.stdin)['engines']['node'])")
+corepack_version=$(cat package.json | python -c "import sys, json; print(json.load(sys.stdin)['devDependencies']['corepack'])")
+curl -sSo "node-v$node_version-linux-x64.tar.xz" https://nodejs.org/dist/v$node_version/node-v$node_version-linux-x64.tar.xz
+mkdir -p "bin/linux/x64/node"
+tar -xJf "node-v$node_version-linux-x64.tar.xz" -C "bin/linux/x64/node"
+rm "node-v$node_version-linux-x64.tar.xz"
+sed -i '/\/bin\//d' -- '.gitignore'
+# Replace bundled npm with the latest version
+npm_version=$(curl -sS 'https://registry.npmjs.org/npm' | python -c "import sys, json; print(json.load(sys.stdin)['dist-tags']['latest'])")
+curl -sSo "npm-$npm_version.tgz" "https://registry.npmjs.org/npm/-/npm-$npm_version.tgz"
+rm -r "bin/linux/x64/node/node-v$node_version-linux-x64/lib/node_modules/npm"
+mkdir "bin/linux/x64/node/node-v$node_version-linux-x64/lib/node_modules/npm"
+tar -xzf "npm-$npm_version.tgz" --strip-components=1 -C "bin/linux/x64/node/node-v$node_version-linux-x64/lib/node_modules/npm"
+rm "npm-$npm_version.tgz"
+# Correct engines
+sed -i "0,/\"npm\": \".*\"/s//\"npm\": \"$npm_version\"/" package.json
+# Ignore dependencies, devDependencies, peerDependencies, and bundleDependencies
+sed -i '/"dependencies": {/,/}/d' -- 'package.json'
+sed -i '/"devDependencies": {/,/}/d' -- 'package.json'
+sed -i '/"peerDependencies": {/,/}/d' -- 'package.json'
+sed -i -z 's|  "bundleDependencies": \[\n    ".*"\n  \]|  "bundleDependencies": \[\]|' -- 'package.json'
+# Replace bundled npm with the latest version
+bin/linux/x64/node/node-v$node_version-linux-x64/bin/node bin/linux/x64/node/node-v$node_version-linux-x64/lib/node_modules/npm/bin/npm-cli.js install --no-offline "corepack@$corepack_version" "npm@$npm_version"
+rm -rf .npm/
+# Remove unnecessary files
+bash --noprofile --norc -e -o pipefail .github/workflows/actions-clean-files.sh
+# Reset workspace
+rm -r "bin/linux/x64/node/node-v$node_version-linux-x64/lib/node_modules"
+rm package-lock.json
+rm node_modules/.package-lock.json
+mv -T "node_modules" "bin/linux/x64/node/node-v$node_version-linux-x64/lib/node_modules"
+git checkout -- 'package.json'
+# Commit
+if [[ $(stat -c%s "bin/linux/x64/node/node-v$node_version-linux-x64/bin/node") -gt 104857600 ]]; then
+  split -b 104857600 --numeric-suffixes=1 --suffix-length=3 bin/linux/x64/node/node-v$node_version-linux-x64/bin/node "bin/linux/x64/node/node-v$node_version-linux-x64/bin/node."
+  rm bin/linux/x64/node/node-v$node_version-linux-x64/bin/node
+fi
+git add "bin/linux/x64/node/node-v$node_version-linux-x64"
+git -c user.name="GitHub" -c user.email="noreply@github.com" commit --author="github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>" -m"Add Linux x64 Node $node_version release artifacts" | sed -n 1p
+git checkout -- '.gitignore'
+if [[ $(git status --porcelain | tee /dev/stderr | head -c1 | wc -c) -ne 0 || $(git clean -dffx | tee /dev/stderr | head -c1 | wc -c) -ne 0 ]]
+  then exit 1
+fi
+
+# Node Windows x64
+curl -sSo "node-v$node_version-win-x64.7z" https://nodejs.org/dist/v$node_version/node-v$node_version-win-x64.7z
+mkdir -p "bin/windows/x64/node"
+7z x -o"bin/windows/x64/node" "node-v$node_version-win-x64.7z" | grep "ing archive"
+rm "node-v$node_version-win-x64.7z"
+sed -i '/\/bin\//d' -- '.gitignore'
+# Replace bundled npm with npm latest
+# Install npm latest with npm 6 because later versions fail to create sane dependency trees
+rm -r "bin/windows/x64/node/node-v$node_version-win-x64/node_modules/npm"
+mkdir "bin/windows/x64/node/node-v$node_version-win-x64/node_modules/npm"
+# Correct engines
+sed -i "0,/\"npm\": \".*\"/s//\"npm\": \"$npm_version\"/" package.json
+# Ignore dependencies, devDependencies, peerDependencies, and bundleDependencies
+sed -i '/"dependencies": {/,/}/d' -- 'package.json'
+sed -i '/"devDependencies": {/,/}/d' -- 'package.json'
+sed -i '/"peerDependencies": {/,/}/d' -- 'package.json'
+sed -i -z 's|  "bundleDependencies": \[\n    ".*"\n  \]|  "bundleDependencies": \[\]|' -- 'package.json'
+# Replace bundled npm with the latest version
+cat bin/linux/x64/node/node-v$node_version-linux-x64/bin/node.* > bin/linux/x64/node/node-v$node_version-linux-x64/bin/node
+chmod +x bin/linux/x64/node/node-v$node_version-linux-x64/bin/node
+bin/linux/x64/node/node-v$node_version-linux-x64/bin/node bin/linux/x64/node/node-v$node_version-linux-x64/lib/node_modules/npm/bin/npm-cli.js install --no-offline "corepack@$corepack_version" "npm@$npm_version"
+rm -rf .npm/
+rm bin/linux/x64/node/node-v$node_version-linux-x64/bin/node
+# Remove unnecessary files
+bash --noprofile --norc -e -o pipefail .github/workflows/actions-clean-files.sh
+# Reset workspace
+rm -r "bin/windows/x64/node/node-v$node_version-win-x64/node_modules"
+rm package-lock.json
+rm node_modules/.package-lock.json
+mv -T "node_modules" "bin/windows/x64/node/node-v$node_version-win-x64/node_modules"
+git checkout -- 'package.json'
+# Commit
+git add "bin/windows/x64/node/node-v$node_version-win-x64"
+git -c user.name="GitHub" -c user.email="noreply@github.com" commit --author="github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>" -m"Add Windows x64 Node $node_version release artifacts" | sed -n 1p
+git checkout -- '.gitignore'
+if [[ $(git status --porcelain | tee /dev/stderr | head -c1 | wc -c) -ne 0 || $(git clean -dffx | tee /dev/stderr | head -c1 | wc -c) -ne 0 ]]
+  then exit 1
+fi
+
+# Electron Linux x64
+electron_version=$(cat package.json | python -c "import sys, json; print(json.load(sys.stdin)['devDependencies']['electron'])")
+# Correct engines
+sed -i "0,/\"npm\": \".*\"/s//\"npm\": \"$npm_version\"/" package.json
+# Ignore dependencies, devDependencies, peerDependencies, and bundleDependencies
+sed -i '/"dependencies": {/,/}/d' -- 'package.json'
+sed -i '/"devDependencies": {/,/}/d' -- 'package.json'
+sed -i '/"peerDependencies": {/,/}/d' -- 'package.json'
+sed -i -z 's|  "bundleDependencies": \[\n    ".*"\n  \]|  "bundleDependencies": \[\]|' -- 'package.json'
+# Install
+cat bin/linux/x64/node/node-v$node_version-linux-x64/bin/node.* > bin/linux/x64/node/node-v$node_version-linux-x64/bin/node
+chmod +x bin/linux/x64/node/node-v$node_version-linux-x64/bin/node
+bin/linux/x64/node/node-v$node_version-linux-x64/bin/node bin/linux/x64/node/node-v$node_version-linux-x64/lib/node_modules/npm/bin/npm-cli.js install --no-offline "electron@$electron_version"
+rm -rf .npm/
+electron_version_target=$(cat node_modules/electron/package.json | python -c "import sys, json; print(json.load(sys.stdin)['version'])")
+export force_no_cache=true
+export npm_config_platform=linux
+export npm_config_arch=x64
+export npm_config_target=$electron_version_target
+bin/linux/x64/node/node-v$node_version-linux-x64/bin/node "node_modules/electron/install.js"
+unset force_no_cache
+unset npm_config_platform
+unset npm_config_arch
+unset npm_config_target
+rm bin/linux/x64/node/node-v$node_version-linux-x64/bin/node
+mkdir -p "bin/linux/x64/electron/electron-v$electron_version_target-linux-x64"
+mv -T node_modules/electron/dist "bin/linux/x64/electron/electron-v$electron_version_target-linux-x64"
+rm -r node_modules/
+rm package-lock.json
+git checkout -- 'package.json'
+if [[ $(stat -c%s "bin/linux/x64/electron/electron-v$electron_version_target-linux-x64/electron") -gt 104857600 ]]; then
+  split -b 104857600 --numeric-suffixes=1 --suffix-length=3 "bin/linux/x64/electron/electron-v$electron_version_target-linux-x64/electron" "bin/linux/x64/electron/electron-v$electron_version_target-linux-x64/electron."
+  rm "bin/linux/x64/electron/electron-v$electron_version_target-linux-x64/electron"
+fi
+if [[ $(stat -c%s "bin/linux/x64/electron/electron-v$electron_version_target-linux-x64/libvk_swiftshader.so") -gt 104857600 ]]; then
+  split -b 104857600 --numeric-suffixes=1 --suffix-length=3 "bin/linux/x64/electron/electron-v$electron_version_target-linux-x64/libvk_swiftshader.so" "bin/linux/x64/electron/electron-v$electron_version_target-linux-x64/libvk_swiftshader.so."
+  rm "bin/linux/x64/electron/electron-v$electron_version_target-linux-x64/libvk_swiftshader.so"
+fi
+sed -i '/\/bin\//d' -- '.gitignore'
+git add "bin/linux/x64/electron"
+git -c user.name="GitHub" -c user.email="noreply@github.com" commit --author="github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>" -m"Add Linux x64 Electron $electron_version_target release artifacts" | sed -n 1p
+git checkout -- '.gitignore'
+if [[ $(git status --porcelain | tee /dev/stderr | head -c1 | wc -c) -ne 0 || $(git clean -dffx | tee /dev/stderr | head -c1 | wc -c) -ne 0 ]]
+  then exit 1
+fi
+
+# Electron Windows x64
+# Correct engines
+sed -i "0,/\"npm\": \".*\"/s//\"npm\": \"$npm_version\"/" package.json
+# Ignore dependencies, devDependencies, peerDependencies, and bundleDependencies
+sed -i '/"dependencies": {/,/}/d' -- 'package.json'
+sed -i '/"devDependencies": {/,/}/d' -- 'package.json'
+sed -i '/"peerDependencies": {/,/}/d' -- 'package.json'
+sed -i -z 's|  "bundleDependencies": \[\n    ".*"\n  \]|  "bundleDependencies": \[\]|' -- 'package.json'
+# Install
+cat bin/linux/x64/node/node-v$node_version-linux-x64/bin/node.* > bin/linux/x64/node/node-v$node_version-linux-x64/bin/node
+chmod +x bin/linux/x64/node/node-v$node_version-linux-x64/bin/node
+bin/linux/x64/node/node-v$node_version-linux-x64/bin/node bin/linux/x64/node/node-v$node_version-linux-x64/lib/node_modules/npm/bin/npm-cli.js install --no-offline "electron@$electron_version"
+rm -rf .npm/
+export force_no_cache=true
+export npm_config_platform=win32
+export npm_config_arch=x64
+export npm_config_target=$electron_version_target
+bin/linux/x64/node/node-v$node_version-linux-x64/bin/node "node_modules/electron/install.js"
+unset force_no_cache
+unset npm_config_platform
+unset npm_config_arch
+unset npm_config_target
+rm bin/linux/x64/node/node-v$node_version-linux-x64/bin/node
+mkdir -p "bin/windows/x64/electron/electron-v$electron_version_target-win32-x64"
+mv -T node_modules/electron/dist "bin/windows/x64/electron/electron-v$electron_version_target-win32-x64"
+rm -r node_modules/
+rm package-lock.json
+git checkout -- 'package.json'
+if [[ $(stat -c%s "bin/windows/x64/electron/electron-v$electron_version_target-win32-x64/electron.exe") -gt 104857600 ]]; then
+  split -b 104857600 --numeric-suffixes=1 --suffix-length=3 "bin/windows/x64/electron/electron-v$electron_version_target-win32-x64/electron.exe" "bin/windows/x64/electron/electron-v$electron_version_target-win32-x64/electron.exe."
+  rm "bin/windows/x64/electron/electron-v$electron_version_target-win32-x64/electron.exe"
+fi
+sed -i '/\/bin\//d' -- '.gitignore'
+git add "bin/windows/x64/electron"
+git -c user.name="GitHub" -c user.email="noreply@github.com" commit --author="github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>" -m"Add Windows x64 Electron $electron_version_target release artifacts" | sed -n 1p
+git checkout -- '.gitignore'
+if [[ $(git status --porcelain | tee /dev/stderr | head -c1 | wc -c) -ne 0 || $(git clean -dffx | tee /dev/stderr | head -c1 | wc -c) -ne 0 ]]
+  then exit 1
+fi
+
+# Node modules
+bash --noprofile --norc -e -o pipefail .github/workflows/actions-npm.sh
+sed -i '/\/node_modules\//d' -- '.gitignore'
+find node_modules/ -mindepth 2 -maxdepth 3 -type f -name 'package.json' -exec bash -c 'path={}; git add -- "${path:0:-13}"; git -c user.name="GitHub" -c user.email="noreply@github.com" commit --author="github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>" -m"Add ${path:13:-13} release artifacts" | sed -n 1p' ';'
+git add -f 'package-lock.json' 'node_modules/.package-lock.json'
+# Execution continues in .github/workflows/artifacts.yml
---
+++ index.bat
@@ -0,0 +1,54 @@
+@echo off
+:: https://learn.microsoft.com/en-us/windows/win32/intl/code-page-identifiers
+chcp 437 >nul
+cd /d "%~dp0"
+goto:start
+
+:git
+for /f "tokens=1,2 delims=:, " %%a in (' find ":" ^< "pagelist.json" ') do (
+  set "%%~a-version=%%~b"
+)
+if not exist "bin\windows\x64\git\PortableGit-%portable-git-short-version%-64-bit\bin\git.exe" (
+  echo Missing git. Downloading it now...
+  curl.exe -#Lo"artifacts.zip" https://github.com/EIGHTFINITE/outdoor-backup/archive/refs/tags/artifacts.zip
+  echo Extracting now. Will give progress update every 500 files...
+  :: https://serverfault.com/a/716619
+  tar.exe -xvf "artifacts.zip" --strip-components=1 -C "." 2>&1 | findstr /n ^^ | findstr "^500: ^[1-9][05]00: ^[1-9][0-9][05]00:"
+  del artifacts.zip
+  for /f "tokens=1,2 delims=:, " %%a in (' find ":" ^< "pagelist.json" ') do (
+    set "%%~a-version=%%~b"
+  )
+)
+bin\windows\x64\git\PortableGit-%portable-git-short-version%-64-bit\bin\git.exe %*
+goto:eof
+
+:start
+set GIT_ASK_YESNO=false
+call:git init .
+call:git config core.autocrlf false
+call:git config core.ignorecase false
+call:git config core.fscache true
+call:git config core.longpaths true
+call:git config diff.renameLimit 0
+call:git config status.renameLimit 0
+call:git config merge.renameLimit 0
+call:git config http.lowSpeedLimit 0
+call:git config http.lowSpeedTime 300
+call:git config http.postBuffer 1048576000
+call:git config pack.threads 1
+call:git config index.threads 0
+call:git config gc.auto 0
+call:git remote add origin https://github.com/EIGHTFINITE/outdoor-backup.call:git 2>nul
+call:git remote set-url origin https://github.com/EIGHTFINITE/outdoor-backup.git
+call:git fetch --force --all --tags
+call:git reset --hard
+call:git checkout -B master refs/remotes/origin/master
+call:git reflog expire --expire-unreachable=all --all
+call:git gc --prune=all
+for /f "tokens=1,2 delims=:, " %%a in (' find ":" ^< "package.json" ') do (
+  set "%%~a-version=%%~b"
+)
+call:git checkout refs/tags/artifacts -- bin/windows/ extensions/ node_modules/ package-lock.json
+call:git clean -ffxe "bin/windows/x64/electron/electron-v%electron-version%-win32-x64/electron.exe" -- bin/ extensions/ node_modules/
+call:git reset -- bin/windows/ extensions/ node_modules/ package-lock.json
+cmd /c "bin\windows\x64\node\node-v%node-version%-win-x64\node" --use_strict index.js
